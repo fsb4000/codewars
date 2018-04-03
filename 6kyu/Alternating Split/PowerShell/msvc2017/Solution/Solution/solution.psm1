e0cea729a29a1ae80d1750ef3569ff3518947c4b
fsb4000 <fsb4000@yandex.ru>
2018-04-03 00:39:11 +0000

    refactoring

--- 6kyu/Alternating Split/PowerShell/msvc2017/Solution/Solution/solution.psm1
+++ 6kyu/Alternating Split/PowerShell/msvc2017/Solution/Solution/solution.psm1
@@ -4,15 +4,16 @@ function Encrypt ([string]$text, [int]$n)
 		return $text;
 	}
 
-	[string]$res = "";
+	[Text.StringBuilder]$res = New-Object -TypeName "System.Text.StringBuilder";
+	$res.Capacity = $text.Length;
 	for ($i = 1; $i -lt $text.Length; $i += 2) { 
-		$res += $text[$i];
+		[void]$res.Append($text[$i]);
 	}
 	for ($i = 0; $i -lt $text.Length; $i += 2) { 
-		$res += $text[$i];
+		[void]$res.Append($text[$i]);
 	}
 
-	return encrypt $res ($n-1);
+	return encrypt $res.ToString() ($n-1);
 }
 
 function Decrypt ([string]$encryptedText, [int]$n)
@@ -21,16 +22,17 @@ function Decrypt ([string]$encryptedText, [int]$n)
 		return $encryptedText;
 	}
 
-	[string]$res = "";
+	[Text.StringBuilder]$res = New-Object -TypeName "System.Text.StringBuilder";
+	$res.Capacity = $encryptedText.Length;
 	for ($i = 0 ; $i -lt $encryptedText.Length; $i++) {
 		if (($i % 2) -eq 1) {
-			$res += $encryptedText[[math]::Truncate($i / 2)];
+			[void]$res.Append($encryptedText[[math]::Truncate($i / 2)]);
 		}
 		else {
-			$res += $encryptedText[[math]::Truncate($i / 2) + [math]::Truncate($encryptedText.Length / 2)];
+			[void]$res.Append($encryptedText[[math]::Truncate($i / 2) + [math]::Truncate($encryptedText.Length / 2)]);
 		}
 	}
-	return decrypt $res ($n - 1);
+	return decrypt $res.ToString() ($n - 1);
 }
 
 Export-ModuleMember -Function Encrypt;
