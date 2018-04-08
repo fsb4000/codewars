cf5c3b22e8e6c5670fa3cdb5268a65fa64dfd849
fsb4000 <fsb4000@yandex.ru>
2018-04-08 23:13:23 +0000

    refactoring

--- 6kyu/Simple string indices/C++/src/solution.cpp
+++ 6kyu/Simple string indices/C++/src/solution.cpp
@@ -14,9 +14,11 @@ int solve(const std::string& str, size_t index)
 	const char openBrace = '(';
 	const char closeBrace = ')';
 	const int error = -1;
+
 	if (str.at(index) != openBrace) {
 		return error;
 	}
+
 	size_t numberOpenBraces = 1;
 	for (size_t i = index + 1ULL; i < str.size(); ++i) {
 		switch (str.at(i)) {
@@ -25,17 +27,18 @@ int solve(const std::string& str, size_t index)
 			break;
 		case closeBrace:
 			--numberOpenBraces;
+			if (numberOpenBraces == 0) {
+				if (i > narrow<size_t>(numeric_limits<int>::max())) {
+					return error;
+				}
+				return narrow<int>(i);
+			}
 			break;
 		default:
 			/* do nothing */
 			break;
 		}
-		if (numberOpenBraces == 0) {
-			if (i > narrow<size_t>(numeric_limits<int>::max())) {
-				return error;
-			}
-			return narrow<int>(i);
-		}
 	}
+
 	return error;
 }
