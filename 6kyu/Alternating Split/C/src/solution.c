edb6833a9e178acd5e397f9d0bb88903518c2ae0
fsb4000 <fsb4000@yandex.ru>
2018-04-09 03:08:30 +0000

    refactoring

--- 6kyu/Alternating Split/C/src/solution.c
+++ 6kyu/Alternating Split/C/src/solution.c
@@ -17,6 +17,7 @@ char* encrypt(const char* text, int n) noexcept
 
 	const size_t len = strlen(text);
 	char* temp = (char*)malloc(len+1);
+	assert (temp != nullptr);
 	size_t current = 0;
 	for (size_t i =1; i < len; i += 2) {
 		temp[current] = text[i];
@@ -44,6 +45,7 @@ char* decrypt(const char* encryptedText, int n) noexcept
 
 	const size_t len = strlen(encryptedText);
 	char* temp = (char*)malloc(len+1);
+	assert (temp != nullptr);
 	size_t current = 0;
 	for (size_t i = 0; i < len; ++i) {
 		if (i % 2 == 1) {
